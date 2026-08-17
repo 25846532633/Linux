ab7a4ccdfca200f3b21e601042db7b5099cd0dd6
GeZhang Cao <2076387719@qq.com>
2026-08-17 19:14:37 +0800

    动态库和静态库

---
+++ 2026_8_17/Mymath/other/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "add.h"
+#include "sub.h"
+
+int main(void)
+{
+    int a = 10;
+    int b = 20;
+
+    printf("add(%d, %d) = %d\n", a, b, add(a, b));
+
+    a = 100;
+    b = 20;
+
+    printf("sub(%d, %d) = %d\n", a, b, sub(a, b));
+
+    return 0;
+}
---
+++ 2026_8_17/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "add.h"
+
+int main()
+{
+	int a = add(2,3);
+    printf("%d\n",a);
+	return 0;
+}
